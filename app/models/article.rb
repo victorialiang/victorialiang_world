e95996464010377035640d8a3da02a2d8630b075
Warren Glasner <wglasner@andrew.cmu.edu>
2019-02-22 15:01:55 -0500

    views functional

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,7 @@ class Article < ApplicationRecord
     has_one :Category
     validates_presence_of :title
     validates_presence_of :content
+
+    scope :alphabetical, -> {order('title')}
+    scope :active, -> {where('active=?', true)}
 end
